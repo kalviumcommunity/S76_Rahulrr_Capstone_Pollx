523731cdae23bf1339b4b982d2139d8775c68764
rahulrr-coder <rahulrr5002@protonmail.com>
2025-04-26 17:48:02 +0530

    add post-api request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const pollroutes = require('./routes/poll');
 require('dotenv').config();
 
 const app = express();
@@ -8,8 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use('/polls', pollroutes);
+
+app.get('/', (req, res) => {
+res.send('Backend server is Live!');    
+})
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log('MongoDB Connected'))
 .catch((err) => console.error('MongoDB Connection Error', err));
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
